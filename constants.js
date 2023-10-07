6edaf19a75057946244301936eee298942809bcc
dragon-quest <106597760+dragon-quest@users.noreply.github.com>
2023-10-07 19:39:35 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = 'AIzaSyCbNedQlNOAQk0PG8mnfpgINIrtlMDJ1xA';
+const API_KEY = 'AIzaSyDHXtA3rZxKFTsi2cbbVOndYmMGPQ0BCLM';
 const SHEET_ID = '1McI_ojVZw8hGaxrLWeOGmUq1mDWSnbpzkp5PzHR0e4I';
